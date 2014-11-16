8b25670c56041538fa8fedcd038918d3a386abb6
Andrew Vos <andrew.vos@gmail.com>
2014-11-16 13:25:49 +0000

    Clean up the style a bit

--- public/scripts/gifs.js
+++ public/scripts/gifs.js
@@ -3,7 +3,7 @@ $(function() {
 });
 
 function setAllToFirstFrameBut(img) {
-  $(".pin img").each(function() {
+  $(".item img").each(function() {
     $(this).attr("src", $(this).data("first-frame"));
   });
   if (img != null) {
@@ -12,14 +12,14 @@ function setAllToFirstFrameBut(img) {
 }
 
 function loadPage() {
-  $(".pin img").on("error", function() {
+  $(".item img").on("error", function() {
     $(this).parent().remove();
   });
 
-  $(".pin").click(function() {
+  $(".item").click(function() {
     setAllToFirstFrameBut($(this).find("img"));
   });
-  $(".pin").hover(function() {
+  $(".item").hover(function() {
     setAllToFirstFrameBut($(this).find("img"));
   }, function() {
     setAllToFirstFrameBut();
