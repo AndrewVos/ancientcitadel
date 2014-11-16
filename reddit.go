08716baed22b87e7cc94836eb3397f8f99ec299a
Andrew Vos <andrew.vos@gmail.com>
2014-11-16 14:09:49 +0000

    Remove some crappy subreddits

--- reddit.go
+++ reddit.go
@@ -7,10 +7,9 @@ func SubReddits() []string {
 		"adventuretimegifs", "animegifs", "communitygifs", "Movie_GIFs", "tvgifs", "gaming_gifs",
 		"AnimalsBeingJerks", "AnimalGIFs", "birdreactiongifs", "CatGifs", "catreactiongifs", "slothgifs",
 		"Puggifs", "celebgifs", "KimJongUnGifs", "SpaceGifs", "physicsgifs", "educationalgifs",
-		"chemicalreactiongifs", "mechanical_gifs", "cargifs", "wobblegifs", "SurrealGifs", "unboxinggifs",
-		"NSFW_WTF", "gifsgonewild", "porn_gifs", "PornGifs", "NSFW_SEXY_GIF", "nsfwcelebgifs",
-		"adultgifs", "NSFW_GIF", "nsfw_gifs", "XXX_Animated_Gifs", "porngif", "cumshot_gifs",
-		"BlowjobGifs", "gifyo", "cutegirlgifs", "Hot_Women_Gifs", "Nsfw_Interesting_Gifs", "randomsexygifs",
+		"chemicalreactiongifs", "mechanical_gifs", "cargifs", "wobblegifs", "SurrealGifs",
+		"gifsgonewild", "porn_gifs", "PornGifs", "NSFW_SEXY_GIF", "nsfwcelebgifs",
+		"adultgifs", "NSFW_GIF", "nsfw_gifs", "porngif", "cutegirlgifs", "Hot_Women_Gifs", "randomsexygifs",
 		"TittyDrop", "boobbounce", "boobgifs",
 	}
 }
