3c1f6517b57ed253d0bad3f46b0eec8cc3fc5db0
Andrew Vos <andrew.vos@gmail.com>
2014-11-30 23:06:44 +0000

    Remove video controls, play on click

--- assets/scripts/gifs.js
+++ assets/scripts/gifs.js
@@ -1,3 +1,11 @@
 $(document).on("click", ".next-page a", function() {
   $(this).slideUp();
 });
+
+$(document).on("click", ".gif", function() {
+  if (this.paused == true) {
+    this.play();
+  } else {
+    this.pause();
+  }
+});
