eba7e88ef85d6a97198f8e03e2e1519aa3020110
Andrew Vos <andrew.vos@gmail.com>
2014-12-07 15:39:10 +0000

    Download MOARRRRR gifs

--- reddit/reddit.go
+++ reddit/reddit.go
@@ -66,44 +66,51 @@ type RedditURL struct {
 	Permalink string
 }
 
-func redditURLs(subReddit subReddit, after string) ([]RedditURL, error) {
-	url := fmt.Sprintf("https://api.reddit.com/r/%v/top.json", subReddit.Name)
-	if after != "" {
-		url += "?after=" + after
-	}
-
-	response, err := http.Get(url)
-	if err != nil {
-		return []RedditURL{}, err
-	}
-	b, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return []RedditURL{}, err
-	}
-	var redditResponse redditResponse
-	err = json.Unmarshal(b, &redditResponse)
-	if err != nil {
-		return []RedditURL{}, err
-	}
-
+func redditURLs(subReddit subReddit, pages int) ([]RedditURL, error) {
+	after := ""
 	var urls []RedditURL
-	for _, child := range redditResponse.Data.Children {
-		url := child.Data.URL
-		if strings.Contains(url, "imgur.com") && !strings.HasSuffix(url, ".gif") {
-			url = url + ".gif"
-		}
-		if strings.Contains(url, "gfycat.com") && !strings.HasSuffix(url, ".gif") {
-			url = strings.Replace(url, "http://gfycat", "http://giant.gfycat", -1)
-			url += ".gif"
+
+	for page := 0; page < pages; page++ {
+		log.Printf("Downloading top urls from /r/%v, page %d\n", subReddit.Name, page+1)
+		url := fmt.Sprintf("https://api.reddit.com/r/%v/top.json", subReddit.Name)
+		if after != "" {
+			url += "?after=" + after
 		}
 
-		urls = append(urls, RedditURL{
-			Work:      subReddit.Work,
-			subReddit: subReddit.Name,
-			Title:     child.Data.Title,
-			URL:       url,
-			Permalink: child.Data.Permalink,
-		})
+		response, err := http.Get(url)
+		if err != nil {
+			return []RedditURL{}, err
+		}
+		b, err := ioutil.ReadAll(response.Body)
+		if err != nil {
+			return []RedditURL{}, err
+		}
+		var redditResponse redditResponse
+		err = json.Unmarshal(b, &redditResponse)
+		if err != nil {
+			return urls, err
+		}
+
+		for _, child := range redditResponse.Data.Children {
+			url := child.Data.URL
+			if strings.Contains(url, "imgur.com") && !strings.HasSuffix(url, ".gif") {
+				url = url + ".gif"
+			}
+			if strings.Contains(url, "gfycat.com") && !strings.HasSuffix(url, ".gif") {
+				url = strings.Replace(url, "http://gfycat", "http://giant.gfycat", -1)
+				url += ".gif"
+			}
+
+			urls = append(urls, RedditURL{
+				Work:      subReddit.Work,
+				subReddit: subReddit.Name,
+				Title:     child.Data.Title,
+				URL:       url,
+				Permalink: child.Data.Permalink,
+			})
+		}
+		log.Println("Sleeping for 2 seconds...")
+		time.Sleep(2 * time.Second)
 	}
 	return urls, nil
 }
@@ -113,14 +120,12 @@ func GetRedditURLs() []RedditURL {
 
 	for _, sr := range subReddits() {
 		log.Printf("Downloading /r/%v\n", sr.Name)
-		urls, err := redditURLs(sr, "")
+		urls, err := redditURLs(sr, 10)
 		if err != nil {
 			log.Println(err)
 			continue
 		}
 		groupedRedditURLs = append(groupedRedditURLs, urls)
-		log.Println("Sleeping for 2 seconds...")
-		time.Sleep(2 * time.Second)
 	}
 
 	longestSetOfURLs := 0
