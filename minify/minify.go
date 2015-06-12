0a1d8fb175f99c8d2da9d1e7e2ad2f4cd4f48e1f
Andrew Vos <andrew.vos@gmail.com>
2015-06-12 16:05:01 +0100

    Cache loaded assets

--- minify/minify.go
+++ minify/minify.go
@@ -1,21 +1,36 @@
 package minify
 
 import (
-	"io"
+	"io/ioutil"
 	"net/http"
 	"os"
 )
+
+var cache = map[string][]byte{}
+
+func getFile(assetPath string) ([]byte, error) {
+	if b, ok := cache[assetPath]; ok {
+		return b, nil
+	}
+	file, err := os.Open(assetPath)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+	b, err := ioutil.ReadAll(file)
+	return b, err
+}
 
 func handler(assetPaths []string, contentType string) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", contentType)
 		for _, assetPath := range assetPaths {
-			file, err := os.Open(assetPath)
+			b, err := getFile(assetPath)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
 			}
-			_, err = io.Copy(w, file)
+			_, err = w.Write(b)
 			if err != nil {
 				http.Error(w, err.Error(), http.StatusInternalServerError)
 				return
