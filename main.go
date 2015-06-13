3e9af8e59e731e352a4dc0bafc242dd7786bd781
Andrew Vos <andrew.vos@gmail.com>
2015-06-13 20:32:02 +0100

    Quiet logging a bit

--- main.go
+++ main.go
@@ -445,7 +445,7 @@ func updateSubReddit(name string, nsfw bool) error {
 				continue
 			}
 
-			fmt.Printf("original: %v\n", redditURL.URL)
+			original := redditURL.URL
 			if strings.Contains(redditURL.URL, "imgur.com") && !strings.HasSuffix(redditURL.URL, ".gif") {
 				redditURL.URL = strings.TrimSuffix(redditURL.URL, ".gifv")
 				redditURL.URL = strings.TrimSuffix(redditURL.URL, ".webm")
@@ -456,7 +456,9 @@ func updateSubReddit(name string, nsfw bool) error {
 				redditURL.URL = strings.Replace(redditURL.URL, "http://gfycat", "http://giant.gfycat", -1)
 				redditURL.URL += ".gif"
 			}
-			fmt.Printf("modified: %v\n", redditURL.URL)
+			if redditURL.URL != original {
+				fmt.Printf("modified %q to %q\n", original, redditURL.URL)
+			}
 
 			sourceURL := "https://reddit.com" + redditURL.Permalink
 
